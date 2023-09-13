767d4609be676e0c81dff1697467fdf5785dc5b6
Ark agrawal <99121920+arkagrawal1@users.noreply.github.com>
2023-09-13 22:37:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,6 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!\n Bhai mujhe text math bhej File bhejdunga link dede mujhe"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append()
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
