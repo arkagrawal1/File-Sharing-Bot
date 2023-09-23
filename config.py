d38c3d328dbc4aeba87b6ebe219428b977826166
Ark agrawal <99121920+arkagrawal1@users.noreply.github.com>
2023-09-24 00:20:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,84 @@
 
+
+
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6611080913:AAGAhl6FnUTAawTfzXD84RmblKPXm4GsJ4s")#
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "28313254"))#
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "09c8a97fe8b643ca22aefceefe37c2a2")#
+
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001639720150"))#-
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "1486687703"))#1486687703
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ark:l8fIAa92s6h47xs7@filegod.xh9illb.mongodb.net/?retryWrites=true&w=majority")#
+DB_NAME = os.environ.get("DATABASE_NAME", "filebot")
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001676466998"))#
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nThis bot is of Files God \n\n---made by @theintruder7")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel to use the bot\n\n ----Made by @theintruder7</b>")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", 'Share for more content')
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", 'True') == 'True'
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!\n Bhai mujhe text math bhej File bhejdunga link dede mujhe"
+
+ADMINS.append(OWNER_ID)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
